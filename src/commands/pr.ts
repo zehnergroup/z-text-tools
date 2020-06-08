3e2a2509dd2847ccd5a55216e86ee871bc89376b
Ksenia Koldaeva <kseniacold@gmail.com>
2020-06-08 09:33:06 -0700

    Create pr command

--- src/commands/pr.ts
+++ src/commands/pr.ts
@@ -2,15 +2,20 @@ import { Config, Feature } from "../types";
 import getCurrentFeature from "../feature/getCurrentFeature";
 import getDBAdapter from "../db/getDBAdapter";
 import pr from "../pr/pr";
+import getWorkingDirectory from "../workingDirectory/getWorkingDirectory";
 
 export const command = "pr";
 export const desc = "Create pull request from the context of current feature";
-export const handler = async () => {
+
+export const handler = async (argv: any) => {
   try {
-    const db = await getDBAdapter();
+    const workingDirectory = await getWorkingDirectory(argv);
+    const db = await getDBAdapter(workingDirectory);
     const config: Config = db.get("config").value();
 
-    const currentFeature: Feature | null = await getCurrentFeature();
+    const currentFeature: Feature | null = await getCurrentFeature(
+      workingDirectory
+    );
 
     if (currentFeature && config) {
       pr(currentFeature, config);
