4ca0e12b6fffd54c354acdbe539a1633312b0b35
Ksenia Koldaeva <kseniacold@gmail.com>
2020-06-07 19:28:19 -0700

    Create feature list command

---
+++ src/commands/feature/list.ts
@@ -0,0 +1,28 @@
+import chalk from "chalk";
+import { Feature } from "../../types";
+import { displayFeatures } from "../../tables";
+
+import getDBAdapter from "../../db/getDBAdapter";
+import getWorkingDirectory from "../../workingDirectory/getWorkingDirectory";
+
+export const command = "list";
+export const desc = "List Exisitng Feature";
+
+export const handler = async (args: any) => {
+  try {
+    const workingDirectory = await getWorkingDirectory(args);
+    const db = await getDBAdapter(workingDirectory);
+    const features: Feature[] = db.get("features").value();
+
+    if (!features || !features.length) {
+      console.log(
+        chalk.yellow(`No features found in ${chalk.gray(workingDirectory)}`)
+      );
+      console.log(`Use ${chalk.cyanBright("z-tools")} feature create <args>\n`);
+    } else {
+      displayFeatures(features);
+    }
+  } catch (error) {
+    console.error(chalk.redBright("Error: "), error);
+  }
+};
