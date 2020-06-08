78dd77ec60a7970b6351171cce24fcb46ce87dbb
Ksenia Koldaeva <kseniacold@gmail.com>
2020-06-08 09:31:14 -0700

    Create branch create

--- src/commands/branch/create.ts
+++ src/commands/branch/create.ts
@@ -2,17 +2,22 @@ import { Config, Feature } from "../../types";
 import getCurrentFeature from "../../feature/getCurrentFeature";
 import getDBAdapter from "../../db/getDBAdapter";
 import branchCheckout from "../../branch/branchCheckout";
+import getWorkingDirectory from "../../workingDirectory/getWorkingDirectory";
 
 export const command = "checkout";
 export const desc = "Checkout or create and cheackout a feature brach";
-export const handle = async () => {
-  const db = await getDBAdapter();
+
+export const handle = async (argv: any) => {
+  const workingDirectory = await getWorkingDirectory(argv);
+  const db = await getDBAdapter(workingDirectory);
   const config: Config = db.get("config").value();
 
-  const currentFeature: Feature | null = await getCurrentFeature();
+  const currentFeature: Feature | null = await getCurrentFeature(
+    workingDirectory
+  );
 
   if (currentFeature && config) {
-    branchCheckout(currentFeature, config);
+    branchCheckout(workingDirectory, currentFeature, config);
   }
 
   // TODO handle otherwise
