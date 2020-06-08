a8b91e04e8a45fe3427d5c953969b4adc1a4e349
Ksenia Koldaeva <kseniacold@gmail.com>
2020-06-07 18:09:02 -0700

    Fix help issue

--- src/z-tools.ts
+++ src/z-tools.ts
@@ -1,13 +1,18 @@
-import * as yargs from "yargs";
+import yargs from "yargs";
+import chalk from "chalk";
+
+const VERSION = require("../package.json").version;
 
 try {
   yargs
     .commandDir("commands")
     .demandCommand()
     .help("help", "Show help for a command")
+    .usage(`Usage:\n${chalk.cyanBright("$0")} <command> [args]`)
     .recommendCommands()
     .scriptName("z-tools")
     .strict()
+    .version("version", "Show version number", VERSION)
     .wrap(yargs.terminalWidth())
     .parse();
 } catch (error) {
