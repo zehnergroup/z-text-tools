5a6765bc7b65bf3085b6f7379dbb18ebaa41d820
Ksenia Koldaeva <kseniacold@gmail.com>
2020-06-18 12:33:48 -0700

    Update pr data with number and URL

--- src/commands/pr.ts
+++ src/commands/pr.ts
@@ -4,6 +4,8 @@ import getDBAdapter from "../db/getDBAdapter";
 import pr from "../pr/pr";
 import getWorkingDirectory from "../workingDirectory/getWorkingDirectory";
 import getConfigFromDB from "../db/getConfigFromDB";
+import comment from "../jira/comment";
+import updatePR from "../pr/updatePR";
 
 export const command = "pr";
 export const desc = "Create pull request from the context of current feature";
@@ -13,13 +15,19 @@ export const handler = async (argv: any) => {
     const workingDirectory = await getWorkingDirectory(argv);
     const db = await getDBAdapter(workingDirectory);
     const config: Config = await getConfigFromDB(workingDirectory, db);
-
     const currentFeature: Feature | null = await getCurrentFeature(
       workingDirectory
     );
 
     if (currentFeature && config) {
-      pr(currentFeature, config);
+      const prData = await pr(currentFeature, config);
+      const { url, number } = prData;
+
+      await updatePR(workingDirectory, { ...currentFeature.pr, url, number });
+
+      // TODO post JIRA Comment
+      // TODO add boolean flag if want to create Jira comment as well
+      // await comment(currentFeature, config);
     }
 
     // TODO handle otherwise
---
+++ src/pr/updatePR.ts
@@ -0,0 +1,23 @@
+import getDBAdapter from "../db/getDBAdapter";
+import { PR } from "../types";
+import errors from "../errors";
+
+export default async (workingDirectory: string, newPR: PR): Promise<void> => {
+  try {
+    const db = await getDBAdapter(workingDirectory);
+    const currentFeatureID: number | null = db.get("currentFeature").value();
+
+    if (!currentFeatureID) {
+      return Promise.reject(new Error(errors.featureCheckout));
+    }
+
+    await db
+      .get("features")
+      .find({ ticket: { id: currentFeatureID } })
+      .update("pr", (oldPR) => ({ ...oldPR, ...newPR }));
+
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
