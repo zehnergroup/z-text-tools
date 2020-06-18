6639d017b0de9855b67a97b306246e6e8121e5f8
Ksenia Koldaeva <kseniacold@gmail.com>
2020-06-18 12:32:38 -0700

    Add jira command

---
+++ src/commands/jira/comment.ts
@@ -0,0 +1,45 @@
+import ora from "ora";
+import chalk from "chalk";
+
+import { Config, Feature } from "../../types";
+import getCurrentFeature from "../../feature/getCurrentFeature";
+import getDBAdapter from "../../db/getDBAdapter";
+import getWorkingDirectory from "../../workingDirectory/getWorkingDirectory";
+import getConfigFromDB from "../../db/getConfigFromDB";
+import comment from "../../jira/comment";
+
+export const command = "comment";
+export const desc = "Add comment to the issue";
+export const builder = {
+  pr: {
+    describe: "Pull request number",
+    demand: false,
+  },
+};
+
+export const handler = async (argv: any) => {
+  const spinner = ora("");
+  try {
+    const workingDirectory = await getWorkingDirectory(argv);
+    const db = await getDBAdapter(workingDirectory);
+    const config: Config = await getConfigFromDB(workingDirectory, db);
+
+    const currentFeature: Feature | null = await getCurrentFeature(
+      workingDirectory
+    );
+
+    if (currentFeature && config) {
+      const {
+        ticket: { projectIdentifier },
+      } = currentFeature;
+      spinner.start(`Creating Jira comment for ${projectIdentifier}`);
+      const commentData = await comment(currentFeature, config, argv.pr);
+      const parsed = JSON.parse(commentData);
+      spinner.succeed(`Created Jira comment for ${projectIdentifier}\n`);
+      console.log(`URL: ${chalk.cyanBright(parsed.self)}`);
+    }
+  } catch (error) {
+    spinner.fail("Creating new feature\n");
+    console.error(chalk.redBright("Error: "), error);
+  }
+};
---
+++ src/jira/comment.ts
@@ -0,0 +1,68 @@
+import { Feature, Config } from "../types";
+import getJiraCommentBody from "../text/getJiraCommentBody";
+import fetch from "node-fetch";
+
+export default async (
+  currentFeature: Feature,
+  config: Config,
+  prNumber?: number
+): Promise<any> => {
+  try {
+    const {
+      themes: { dev: devThemeID, prod: prodThemeID },
+      ticket: { projectIdentifier },
+      pr: { number: prNumberFromFeature },
+    } = currentFeature;
+
+    const {
+      shopify: {
+        base: { dev: shopifyDevBaseURL, prod: shopifyProdBaseURL },
+        editor,
+        hash,
+      },
+      jira: { base: baseJiraURL, token, email },
+      github: { owner, repo },
+    } = config;
+
+    if (prNumberFromFeature && prNumber) {
+      const jiraCommentBody: string = getJiraCommentBody(
+        prNumberFromFeature || prNumber,
+        owner,
+        repo,
+        shopifyDevBaseURL,
+        shopifyProdBaseURL,
+        devThemeID,
+        prodThemeID,
+        editor,
+        hash
+      );
+      const bodyData = {
+        body: jiraCommentBody,
+      };
+
+      return fetch(
+        `${baseJiraURL}/rest/api/2/issue/${projectIdentifier}/comment`,
+        {
+          method: "POST",
+          headers: {
+            ["Authorization"]: `Basic ${Buffer.from(
+              `${email}:${token}`
+            ).toString("base64")}`,
+            ["Accept"]: "application/json",
+            ["Content-Type"]: "application/json",
+          },
+          body: JSON.stringify(bodyData),
+        }
+      )
+        .then((response) => {
+          return response.text();
+        })
+        .catch((err) => console.error(err));
+    }
+
+    // TODO handle if don't have PR number
+    return Promise.resolve();
+  } catch (error) {
+    console.error(error);
+  }
+};
